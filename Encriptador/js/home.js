d18db059a302e6e6eeaf7ce547286389b57b5241
Nahuel0 <71360472+Nahuel0@users.noreply.github.com>
2023-01-02 11:31:09 -0300

    Add files via upload
    Se subio las ultimas modificaciones de la pagina

--- Encriptador/js/home.js
+++ Encriptador/js/home.js
@@ -1,6 +1,5 @@
-
-
 function obtenerTexto(){
+
     let texto = document.getElementById('texto');
     document.getElementById("encriptar").addEventListener('click',()=>
     {
@@ -14,8 +13,10 @@ function obtenerTexto(){
 
 function encriptar(texto){
     let text = texto.value
+
     let cadenaEncriptada = "";
-    if(text.length>0){
+
+    if(text.length>0 && text == text.toLowerCase()){
         
         let rectangulo = document.getElementById("rectangulo");
         
@@ -39,6 +40,7 @@ function encriptar(texto){
         rectangulo.innerHTML = `<p class="text-encriptado">${cadenaEncriptada}</p>`;
         rectangulo.innerHTML += `<button class="button-copy" id="copy">Copiar</button>`;
 
+        //Le damos la funcionalidad al boton de copiar, que copie en portapapeles
         document.getElementById("copy").addEventListener('click',()=>{
             navigator.clipboard.writeText(cadenaEncriptada);
 
@@ -46,31 +48,76 @@ function encriptar(texto){
         
 
     }else{
-        alert("No se encontro texto");
+        alert("No se encontro texto o estas usando mayusculas");
     }
 }
 
 function desencriptar(texto){
-    let text = texto.value
+    let text = texto.value + "."; 
     let cadenaEncriptada = "";
     if(text.length>0){
         
         let rectangulo = document.getElementById("rectangulo");
         
-        for(let i = 0; i<text.length;i++){
+        let i = 0;
+        while(i<=text.length && text !=""){
             switch(text[i]){
                 case "a":
-                    cadenaEncriptada+="ai";break;
+                    if(i == text.indexOf("ai")){
+                        cadenaEncriptada+="a";
+                        text = text.substring(i+2,text.length);
+                        i=0;
+                    }else{
+                        cadenaEncriptada+="a";
+                        i++; 
+                    };;break;
+                    
                 case "e":
-                    cadenaEncriptada+="enter";break;
+                    if(i == text.indexOf("enter")){
+                    cadenaEncriptada+="e";
+                    
+                    text = text.substring(i+5,text.length);
+                    i=0;
+                    }else{
+                        cadenaEncriptada+="e";
+                        i++;      
+                    }
+                    
+                    ;break;
                 case "i":
-                    cadenaEncriptada+="imes";break;
+                    if(i == text.indexOf("imes")){
+                        cadenaEncriptada+="i";
+                        text = text.substring(i+4,text.length);
+                        i=0;
+                    }else{
+                        cadenaEncriptada+="i";
+                        i++; 
+                    };break;
                 case "o":
-                    cadenaEncriptada+="ober";break;
+                    if(i == text.indexOf("ober")){
+                        cadenaEncriptada+="o";
+                        text = text.substring(i+4,text.length);
+                        i=0;
+                    }else{
+                        cadenaEncriptada+="o";
+                        i++; 
+                    };;break;
                  case "u":
-                    cadenaEncriptada+="ufat";break;
+                    if(i == text.indexOf("ufat")){
+                        cadenaEncriptada+="u";
+                        text = text.substring(i+4,text.length);
+                        i=0;
+                    }else{
+                        cadenaEncriptada+="u";
+                        i++; 
+                    };
+                    break;
+                case ".":
+                    i = text.length+1;
+                ;break
                 default:
                     cadenaEncriptada+=text[i];
+                    i++;break;
             }
         }
         
@@ -87,3 +134,4 @@ function desencriptar(texto){
         alert("No se encontro texto para desencriptar");
     }
 }
+
